e9009c6850998bfcb6532152c1d6c9a0c8a52b13
rishabhjaincoder <rishabhjain.rdps@gmail.com>
2020-01-10 01:29:05 +0530

    made few changes

--- email address text scrapper.py
+++ email address text scrapper.py
@@ -4,7 +4,7 @@ import re
 
 text = 'Email addresses of jims students are:- Rishabhjain.rdps@gmail.com jain.naman@jimsindia.org ravneetsingh001@jimsindia.org';
 
-pattern = re.compile('[a-zA-Z0-9\.]+\@[a-zA-Z0-9]+\.[a-zA-Z]+') 
+pattern = re.compile('[a-zA-Z0-9.-_]+\@[a-zA-Z0-9]+\.[a-zA-Z]+') 
 
 result = pattern.findall(text)
 
